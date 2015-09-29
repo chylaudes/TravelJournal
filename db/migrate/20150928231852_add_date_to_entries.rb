4a35489c032a3dcd5ca2f7e8f8d0c7f348c034dd
MattRodigheri <mrodigheri@gmail.com>
2015-09-28 20:16:56 -0700

    fixed homepage and deleted some migrations with errors

--- db/migrate/20150928231852_add_date_to_entries.rb
+++
@@ -1,5 +0,0 @@
-class AddDateToEntries < ActiveRecord::Migration
-  def change
-    add_column :entries, :date, :date
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928211013) do
+ActiveRecord::Schema.define(version: 20150928232131) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "date"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150928211013) do
     t.string   "destination"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "location"
   end
 
 end
